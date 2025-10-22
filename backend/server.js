1a1cf022e2b7da10519fb4f588f51f7b719fd051
DemonicEmperor <kanitimaanas@gmail.com>
2025-10-22 11:56:54 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.post(`${process.env.REACT_APP_API_URL}/submit`, async (req, res) => {
+app.post("/submit", async (req, res) => {
   const { name, email, phone, domain } = req.body;
 
   if (!name || !email || !phone) {
